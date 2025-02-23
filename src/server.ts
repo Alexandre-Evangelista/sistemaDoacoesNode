6427cef4f0cdccc7d74db98294ca2e4ee443cb77
Kahe Mikáyas <kahe.gohan@gmail.com>
2025-02-23 20:51:12 -0300

    Configurando express, cors, npx

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+
+import express, { Request, Response, NextFunction, response } from 'express';
+import cors from "cors";
+
+
+const app = express()
+
+app.use(cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+  }));
+
+app.get('/',((req: Request, response:Response)=>{
+    response.send("teste")
+
+}))
+const PORT = 3000;
+app.listen(PORT,()=>{
+
+    console.log("Servidor rodando na porta ",PORT);
+
+})

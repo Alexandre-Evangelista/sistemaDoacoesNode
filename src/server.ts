518f9601da292f2e689e1c768a17413f93f2903f
Kahe Mikáyas <kahe.gohan@gmail.com>
2026-01-26 21:43:54 -0300

    Add get to images

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import ongRouter from './routes/ONG/ongRoutes.js';
 import usuarioRouter from './routes/usuarioRoutes.js';
 import campanhaRouter from './routes/campanhaRoute.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import doacaoRouter from './routes/Doacao/DoacaoRoute.js';
 dotenv.config();
 const app = express()
@@ -20,6 +23,16 @@ app.use("/",campanhaRouter);
 app.use("/",usuarioRouter);
 app.use("/",ongRouter);
 app.use("/",doacaoRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log(
+  path.resolve(__dirname, '..', 'images')
+);
+app.use(
+  '/uploads',
+  express.static(path.resolve(__dirname, '..', 'images'))
+);
 
 app.get('/',((req: Request, response:Response)=>{
     response.send("teste")
